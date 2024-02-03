09bf7f7e05a665223b1585cea21fac32a3b00692
ezoltech <ezramamo13@gmail.com>
2024-02-03 20:16:10 +0300

    fixing some errors

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const cors = require("cors");
+// const cors = require("cors");
 const http = require("http");
 const { PrismaClient } = require("@prisma/client");
 
@@ -13,9 +13,9 @@ const content = require("./controller/contentController");
 
 const app = express();
 const prisma = new PrismaClient();
-const corsOptions = {
-  origin: ["http://localhost:5173", "http://localhost:5174"],
-};
+// const corsOptions = {
+//   origin: ["http://localhost:5173", "http://localhost:5174"],
+// };
 app.use(cors(corsOptions));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
