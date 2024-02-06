05b1acf6baecb6306399185f1dcaef9b416c9384
ezoltech <ezramamo13@gmail.com>
2024-02-06 20:48:24 +0300

    fixing

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require("express");
-// const cors = require("cors");
+const cors = require("cors");
 const http = require("http");
 const { PrismaClient } = require("@prisma/client");
 
